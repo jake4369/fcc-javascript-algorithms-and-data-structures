f9d3b856a446acfb76200512d88025290b189286
Jake Samuels <samuels242@googlemail.com>
2022-07-16 14:07:24 +0100

    Understand Where an Object’s Prototype Comes From

--- object-oriented-programming/index.js
+++ object-oriented-programming/index.js
@@ -183,3 +183,13 @@ Dog.prototype = {
     console.log(`My name is ${this.name}`);
   },
 };
+
+// ========== Understand Where an Object’s Prototype Comes From ==========
+
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// let beagle = new Dog("Snoopy");
+
+// console.log(Dog.prototype.isPrototypeOf(beagle));
