64bfb9151b932292d19b683fc09eb9bb3c6c1428
Jake Samuels <samuels242@googlemail.com>
2022-07-16 12:40:07 +0100

    Create a method on an Object

--- object-oriented-programming/index.js
+++ object-oriented-programming/index.js
@@ -7,10 +7,25 @@
 
 // ========== Use Dot Notation to Access the Properties of an Object ==========
 
+// let dog = {
+//   name: "Spot",
+//   numLegs: 4,
+// };
+
+// console.log(dog.name);
+// console.log(dog.numLegs);
+
+// ========== Create a method on an Object ==========
+
 let dog = {
   name: "Spot",
   numLegs: 4,
+  //   sayLegs: function () {
+  //     return "This dog has " + dog.numLegs + " legs.";
+  //   },
+  sayLegs() {
+    return `This dog has ${dog.numLegs} legs.`;
+  },
 };
 
-console.log(dog.name);
-console.log(dog.numLegs);
+dog.sayLegs();
