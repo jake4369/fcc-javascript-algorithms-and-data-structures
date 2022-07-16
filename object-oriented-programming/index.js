0f54bd7b9425751bef92ba8224b0611f4492683c
Jake Samuels <samuels242@googlemail.com>
2022-07-16 13:28:59 +0100

    Understanding the Constructor Property

--- object-oriented-programming/index.js
+++ object-oriented-programming/index.js
@@ -108,21 +108,31 @@
 
 // ========== Iterate Over All Properties ==========
 
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// Dog.prototype.numLegs = 4;
+
+// let beagle = new Dog("Snoopy");
+
+// let ownProps = [];
+// let prototypeProps = [];
+
+// for (let property in beagle) {
+//   if (beagle.hasOwnProperty(property)) {
+//     ownProps.push(property);
+//   } else {
+//     prototypeProps.push(property);
+//   }
+// }
+
+// ========== Understanding the Constructor Property ==========
+
 function Dog(name) {
   this.name = name;
 }
 
-Dog.prototype.numLegs = 4;
-
-let beagle = new Dog("Snoopy");
-
-let ownProps = [];
-let prototypeProps = [];
-
-for (let property in beagle) {
-  if (beagle.hasOwnProperty(property)) {
-    ownProps.push(property);
-  } else {
-    prototypeProps.push(property);
-  }
+function joinDogFraternity(candidate) {
+  return candidate.constructor === Dog;
 }
