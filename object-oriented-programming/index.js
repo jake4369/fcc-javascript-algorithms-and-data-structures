155c8a5b9c968e89eff54dc45e7924e76629d65d
Jake Samuels <samuels242@googlemail.com>
2022-07-16 14:49:24 +0100

    Set the Child's Prototype to an Instance of the Parent

--- object-oriented-programming/index.js
+++ object-oriented-programming/index.js
@@ -235,6 +235,20 @@ function Dog(name) {
 
 // ========== Inherit Behaviors from a Supertype ==========
 
+// function Animal() {}
+
+// Animal.prototype = {
+//   constructor: Animal,
+//   eat: function () {
+//     console.log("nom nom nom");
+//   },
+// };
+
+// let duck = Object.create(Animal.prototype);
+// let beagle = Object.create(Animal.prototype);
+
+// ========== Set the Child's Prototype to an Instance of the Parent ==========
+
 function Animal() {}
 
 Animal.prototype = {
@@ -244,5 +258,8 @@ Animal.prototype = {
   },
 };
 
-let duck = Object.create(Animal.prototype);
-let beagle = Object.create(Animal.prototype);
+function Dog() {}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
